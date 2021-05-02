a1e92e5016ad66b699555647419b075ac17e7fa5
reactionaryzebra <reactionaryzebra@gmail.com>
2021-05-01 20:54:15 -0700

    Add position field to table

--- src/components/LeagueTable/LeagueTable.jsx
+++ src/components/LeagueTable/LeagueTable.jsx
@@ -1,40 +1,11 @@
 import React from "react";
 
-const dummyData = [
-  {
-    name: "Wolves",
-    points: 80,
-    wins: 15,
-    draws: 5,
-    losses: 10,
-    goalsFor: 66,
-    goalsAgainst: 58
-  },
-  {
-    name: "Brighton",
-    points: 77,
-    wins: 12,
-    draws: 5,
-    losses: 10,
-    goalsFor: 66,
-    goalsAgainst: 58
-  },
-  {
-    name: "Newcastle",
-    points: 70,
-    wins: 15,
-    draws: 5,
-    losses: 10,
-    goalsFor: 66,
-    goalsAgainst: 58
-  }
-];
-
 function LeagueTable({ table }) {
   return (
     <table>
       <thead>
         <tr>
+          <th>Pos</th>
           <th>Club</th>
           <th>Pts</th>
           <th>W</th>
@@ -47,7 +18,7 @@ function LeagueTable({ table }) {
       <tbody>
         {table.map((team, index) => (
           <tr key={team.name}>
-            {/* <td>{index + 1}</td> */}
+            <td>{index + 1}</td>
             <td>{team.name}</td>
             <td>{team.points}</td>
             <td>{team.wins}</td>
