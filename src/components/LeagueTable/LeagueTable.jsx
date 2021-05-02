73373cb1e514f5817ed4ecc0e39d959d46024a3a
reactionaryzebra <reactionaryzebra@gmail.com>
2021-05-01 17:15:15 -0700

    Add proper html elements to table

--- src/components/LeagueTable/LeagueTable.jsx
+++ src/components/LeagueTable/LeagueTable.jsx
@@ -30,30 +30,34 @@ const dummyData = [
   }
 ];
 
-function LeagueTable({ table = dummyData }) {
+function LeagueTable({ table }) {
   return (
     <table>
-      <tr>
-        <th>Club</th>
-        <th>Pts</th>
-        <th>W</th>
-        <th>D</th>
-        <th>L</th>
-        <th>GF</th>
-        <th>GA</th>
-      </tr>
-      {dummyData.map((team, index) => (
-        <tr key={team.name}>
-          <td>{index + 1}</td>
-          <td>{team.name}</td>
-          <td>{team.points}</td>
-          <td>{team.wins}</td>
-          <td>{team.draws}</td>
-          <td>{team.losses}</td>
-          <td>{team.goalsFor}</td>
-          <td>{team.goalsAgainst}</td>
+      <thead>
+        <tr>
+          <th>Club</th>
+          <th>Pts</th>
+          <th>W</th>
+          <th>D</th>
+          <th>L</th>
+          <th>GF</th>
+          <th>GA</th>
         </tr>
-      ))}
+      </thead>
+      <tbody>
+        {table.map((team, index) => (
+          <tr key={team.name}>
+            {/* <td>{index + 1}</td> */}
+            <td>{team.name}</td>
+            <td>{team.points}</td>
+            <td>{team.wins}</td>
+            <td>{team.draws}</td>
+            <td>{team.losses}</td>
+            <td>{team.goalsFor}</td>
+            <td>{team.goalsAgainst}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
