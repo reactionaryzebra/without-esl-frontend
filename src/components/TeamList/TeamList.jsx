9ee22628aa6aac9a513c0fe33280286c014ecbff
reactionaryzebra <reactionaryzebra@gmail.com>
2021-05-03 13:36:00 -0700

    Add a label to TeamList and center styling

--- src/components/TeamList/TeamList.jsx
+++ src/components/TeamList/TeamList.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import traitors from "../../constants/traitors.json";
 import TeamLogo from "../TeamLogo/TeamLogo.jsx";
-
-const listStyle = {
-  padding: "0 15rem",
-  display: "flex",
-  justifyContent: "space-around"
-};
+import "./TeamList.css";
 
 function TeamList({ onSelect, selectedTeams }) {
   return (
-    <div style={listStyle}>
-      {traitors.map(traitor => (
-        <TeamLogo
-          key={traitor.id}
-          team={traitor}
-          onSelect={onSelect}
-          isSelected={selectedTeams.includes(traitor.id)}
-        />
-      ))}
+    <div className="team-list">
+      <label>Select Traitors to Ban or Unban</label>
+      <div className="list">
+        {traitors.map(traitor => (
+          <TeamLogo
+            key={traitor.id}
+            team={traitor}
+            onSelect={onSelect}
+            isSelected={selectedTeams.includes(traitor.id)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
